2c8b89981b826703b241bb6fd1a10679fcf0f07a
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-03-15 17:48:26 -0700

    Write api get fxnality

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,14 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { insert } = require('../lib/models/Snack');
+
+// this is just for the test while we don't have the order model built out
+// async function createSnack({food, type}) {
+//   const {rows} = await pool.query(
+//     'INSERT INTO'
+//   )
+// }
 
 describe('anyapi routes', () => {
   beforeEach(() => {
@@ -23,5 +31,19 @@ describe('anyapi routes', () => {
       food: 'Chocolate',
       type: 'Candy',
     });
+  });
+
+  it('gets all snacks', async () => {
+    await insert({ food: 'Chocolate', type: 'Candy' });
+
+    const res = await request(app).get('/api/v1/snacks');
+
+    expect(res.body).toEqual([
+      {
+        id: expect.any(String),
+        food: 'Chocolate',
+        type: 'Candy',
+      },
+    ]);
   });
 });
